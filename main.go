2754cc7144df3045e3104232a53007edb44f8196
stepup <ganuj0302@gmail.com>
2024-07-13 16:10:53 +0530

    Golang installation & configuration - 1
    Golang installation & configuration - 1

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hi there >>>>>>>>> ")
+}
+
+// GOROOT - where you have installed go
+// GOPath - where every go repos goes
+// where go -> then do this --> cd C:\Program Files\Go\
+// go build main.go
+// go run main.go
+// go build -o helloWorld main.go
